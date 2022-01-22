644307bb1bdfd1e4067d4f9e34fc17e7e387dc4c
Jungbin Oh <benoh0906@gmail.com>
2022-01-22 11:32:02 -0800

    Update ballot.sol

--- ballot.sol
+++ ballot.sol
@@ -2,13 +2,18 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-    
+
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
-    
+
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-    
+
+/* getMessage() is actually unnecessary as message variable will auto create message() funcgtion as defauly */
+    function getMessage() public view returns (string){
+        return message
+    } 
 }
+

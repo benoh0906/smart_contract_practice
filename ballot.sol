bacd00c1dba5a14f0b314c5a6125314bdb0c3ba5
Jungbin Oh <benoh0906@gmail.com>
2022-01-24 22:56:32 -0800

    Update ballot.sol

--- ballot.sol
+++ ballot.sol
@@ -17,3 +17,16 @@ contract Inbox {
     } 
 }
 
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    } 
+}
